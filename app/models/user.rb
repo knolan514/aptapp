b52233f2bbcdfd86ee0647776df1e39c0b8f714b
learn <learn@learn03.local>
2016-09-20 16:33:21 -0700

    added avatar to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
 
   has_many :apartments
+
+  has_attached_file :avatar, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  validates_attachment :avatar, presence: true,
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+    size: { in: 0..10.megabytes }
 end
